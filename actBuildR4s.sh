ab5f29ca52a31f238f2177973f5d9180c6908594
juebanlin <juebanlin@gmail.com>
2021-03-03 16:39:13 +0800

    init

---
+++ actBuildR4s.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# install act
+# curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+
+#github_runner=ubuntu-latest
+github_runner=ubuntu-20.04
+#github_runner=ubuntu-18.04
+#github_runner=ubuntu-16.04
+
+#dockerImg=nektos/act-environments-ubuntu:18.04
+dockerImg=catthehacker/ubuntu:runner-20.04
+
+#act -v --privileged --platform ${github_runner}=${dockerImg}
+act workflow_dispatch -W ./.github/workflows/build_Lede_R4S.yml -v -P ${github_runner}=${dockerImg}
