e313ae3daff79ec392161d4d023572e5a3c71a7e
juebanlin <juebanlin@gmail.com>
2021-03-08 10:14:20 +0800

    1

--- actBuildR4s.sh
+++ actBuildR4s.sh
@@ -12,7 +12,7 @@ github_runner=ubuntu-20.04
 dockerImg=catthehacker/ubuntu:runner-20.04
 
 #act -v --privileged --platform ${github_runner}=${dockerImg}
-#act workflow_dispatch -W ./.github/workflows/build_Lede_R4S.yml -v -P ${github_runner}=${dockerImg}
+act workflow_dispatch -W ./.github/workflows/build_Lede_R4S.yml -v -P ${github_runner}=${dockerImg}
 
 #绑定工作目录挂载进去编译,并设置openwrt目录在此工作目录下
-act workflow_dispatch -W ./.github/workflows/build_Lede_R4S.yml -v -P ${github_runner}=${dockerImg} -s BIND_IN_WORKSPACE=true -b
+#act workflow_dispatch -W ./.github/workflows/build_Lede_R4S.yml -v -P ${github_runner}=${dockerImg} -s BIND_IN_WORKSPACE=true -b
