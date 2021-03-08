4e5d6515284de70039cd6cb861dec923222d0019
juebanlin <juebanlin@gmail.com>
2021-03-08 16:14:04 +0800

    ok

--- actBuild.sh
+++ actBuild.sh
@@ -3,6 +3,17 @@
 # install act
 # curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
 
+ymlList=$(ls .github/workflows | grep '.yml')
+ymlList=(${ymlList// / })
+
+for (( i = 0; i < ${#ymlList[*]}; ++i )); do
+    echo "[$i]: ${ymlList[i]}"
+done
+
+read -p "选择要编译的工作流: " no ;
+yml="${ymlList[no]}"
+echo "当前选择: $yml"
+
 #github_runner=ubuntu-latest
 github_runner=ubuntu-20.04
 #github_runner=ubuntu-18.04
@@ -11,8 +22,7 @@ github_runner=ubuntu-20.04
 #dockerImg=nektos/act-environments-ubuntu:18.04
 dockerImg=catthehacker/ubuntu:runner-20.04
 
-#act -v --privileged --platform ${github_runner}=${dockerImg}
-act workflow_dispatch -W ./.github/workflows/build_Lede_R4S.yml -v -P ${github_runner}=${dockerImg}
+act workflow_dispatch -W ./.github/workflows/${yml} -v -P ${github_runner}=${dockerImg}
 
 #绑定工作目录挂载进去编译,并设置openwrt目录在此工作目录下
 #act workflow_dispatch -W ./.github/workflows/build_Lede_R4S.yml -v -P ${github_runner}=${dockerImg} -s BIND_IN_WORKSPACE=true -b
